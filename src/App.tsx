3fcfc6af83039c9708c1853dafdc0ed983568a0e
Ritsuki333 <fujiwarametaritsuki@gmail.com>
2025-04-08 10:33:31 +0900

    初回コミット：React + Tailwind 初期構成

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-600">
+      PDCA日記アプリ
+    </h1>
+  );
+}
+
+export default App;
---
+++ src/src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+
+const LoginPage = () => {
+    return (
+        <div className="text-2xl font-bold">
+            <h1>ログインページ</h1>
+        </div>
+    )
+}
+
+export default LoginPage;
